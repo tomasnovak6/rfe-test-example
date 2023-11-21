e1705dc37e0b8c9958c187ec47c4f4e94a692672
Tomas Novak <tomas.novak@k-business.com>
2023-11-21 23:38:01 +0100

    Uniques

--- src/components/duplicates/Duplicates.tsx
+++ src/components/duplicates/Duplicates.tsx
@@ -1,23 +1,40 @@
 import {useEffect, useState} from "react";
 
 export function Duplicates() {
-    const [numberArr, setNumberArr] = useState([0]);
+    const [dupliciteArr, setDupliciteArr] = useState([0]);
+    const [uniqueArr, setUniqueArr] = useState([0]);
 
-    useEffect((): void => {
+    useEffect(() => {
         const setFoundDuplicates = (): void => {
             const all: number[] = [2, 4, 6, 7, 4, 3, 5, 2];
+
             const duplicates: number[] = all.filter((item, index) => all.indexOf(item) !== index);
-            setNumberArr(duplicates);
+            setDupliciteArr(duplicates);
+
+            const uniques: number[] = all.filter((item, index) => all.indexOf(item) === index);
+            setUniqueArr(uniques);
         }
         setFoundDuplicates();
-    });
+    }, []);
 
     return (
         <div className="row">
             <div className="col-4">
-                {numberArr && numberArr.length > 0 && numberArr[0] !== 0 &&
+                {dupliciteArr && dupliciteArr.length > 0 && dupliciteArr[0] !== 0 &&
                     <ul>
-                        {numberArr.map((item, index) => {
+                        {dupliciteArr.map((item, index) => {
+                            return (
+                                <li key={index}>{item}</li>
+                            )
+                        }
+                        )}
+                    </ul>
+                }
+            </div>
+            <div className="col-4">
+                {uniqueArr && uniqueArr.length > 0 && uniqueArr[0] !== 0 &&
+                    <ul>
+                        {uniqueArr.map((item, index) => {
                             return (
                                 <li key={index}>{item}</li>
                             )
